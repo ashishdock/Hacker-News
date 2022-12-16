209a6daddc5e86a84a3a57180f18375ac45d135f
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-16 20:50:53 +0530

    Completed. Also updated settings to run on mac too along with iOS.

---
+++ Hacker News/Views/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Ashish Sharma on 12/16/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts) {
+                post in
+                NavigationLink(
+                    destination: DetailView(url: post.url),
+                    label: {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
+                    })
+                
+            }
+            .navigationBarTitle("Hacker News")
+        }
+        .onAppear(perform: {
+            networkManager.fetchData()
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+/*let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
+*/
---
+++ Hacker News/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  Hacker News
+//
+//  Created by Ashish Sharma on 12/16/2022.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
